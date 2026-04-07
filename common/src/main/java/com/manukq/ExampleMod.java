7e1978eafdbb9e4cd3ff8e5d396ae2c7d201338f
manukek <thezhake@gmail.com>
2026-04-07 21:49:45 +0500

    QuickFind: Initial Commit

---
+++ common/src/main/java/com/manukq/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.manukq;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "quickfind";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
